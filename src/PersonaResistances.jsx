8de2279997e4962d978df1b8a6f95bfea3fb6e29
Aaron Heuckroth <a.heuckroth@gmail.com>
2017-04-24 22:04:05 -0400

    update SVG icons, add nul/rpl/wk/dr

--- src/PersonaResistances.jsx
+++ src/PersonaResistances.jsx
@@ -2,57 +2,35 @@ import React from 'react';
 import { Table } from 'reactstrap';
 import TypeIcon from './icons/TypeIcon.jsx';
 
-const PersonaResistances = ({ resistances }) => (
-  <Table size="sm" responsive>
-    <thead>
-      <tr>
-        <th>
-          <TypeIcon type='phys' />
-        </th>
-        <th>
-          <TypeIcon type='gun' />
-        </th>
-        <th>
-          <TypeIcon type='fire' />
-        </th>
-        <th>
-          <TypeIcon type='ice' />
-        </th>
-        <th>
-          <TypeIcon type='elec' />
-        </th>
-        <th>
-          <TypeIcon type='wind' />
-        </th>
-        <th>
-          <TypeIcon type='psy' />
-        </th>
-        <th>
-          <TypeIcon type='nuke' />
-        </th>
-        <th>
-          <TypeIcon type='bless' />
-        </th>
-        <th>
-          <TypeIcon type='curse' />
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{resistances.phys}</td>
-        <td>{resistances.gun}</td>
-        <td>{resistances.fire}</td>
-        <td>{resistances.ice}</td>
-        <td>{resistances.electric}</td>
-        <td>{resistances.wind}</td>
-        <td>{resistances.psy}</td>
-        <td>{resistances.nuclear}</td>
-        <td>{resistances.bless}</td>
-        <td>{resistances.curse}</td>
-      </tr>
-    </tbody>
-  </Table>
-)
+const resistList = [
+  'phys',
+  'gun',
+  'fire',
+  'ice',
+  'electric',
+  'wind',
+  'psy',
+  'nuclear',
+  'bless',
+  'curse',
+]
+
+const PersonaResistances = ({ resistances }) => {
+  console.log(resistances);
+  return (
+    <Table size="sm" responsive>
+      <thead style={{ background: '#252525' }}>
+        <tr>
+          {resistList.map(resist => <th><TypeIcon type={resist} /></th>)}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {resistList.map(resist => <td><TypeIcon type={resistances[resist]}/></td>)}
+        </tr>
+      </tbody>
+    </Table>
+  )
+}
 
 export default PersonaResistances;
--- src/icons/TypeIcon.jsx
+++ src/icons/TypeIcon.jsx
@@ -1,8 +1,22 @@
 import nukeSvg from './nuke.svg';
+import fireSvg from './fire.svg';
+import elecSvg from './elec.svg';
+import blessSvg from './bless.svg';
+import curseSvg from './curse.svg';
+import iceSvg from './ice.svg';
+import windSvg from './wind.svg';
+import psySvg from './psy.svg';
+import gunSvg from './gun.svg';
+import physSvg from './phys.svg';
+import wkSvg from './Wk.svg';
+import strSvg from './Str.svg';
+import drSvg from './Dr.svg';
+import nullSvg from './Null.svg';
+import rplSvg from './Rpl.svg';
 import React from 'react';
 import { Tooltip } from 'reactstrap';
 
-class TypeIcon extends React.Component {
+class TooltipIcon extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
@@ -18,20 +32,88 @@ class TypeIcon extends React.Component {
   }
 
   render() {
-    const { type } = this.props;
-    switch(type) {
-      case ('nuke'):
-        return (
-          <div>
-            <img id="nukeIcon" src={nukeSvg} />
-            <Tooltip placement="right" isOpen={this.state.tooltipOpen} target="nukeIcon" toggle={this.toggle}>
-              Nuclear
-            </Tooltip>
-          </div>
-        );
-      default:
-        return <div>{type}</div>;
-    }
+    const { tipText, icon } = this.props;
+    const id=`${tipText}-icon-hover`
+    return (
+      <div>
+        <img style={{ width: '50px' }} id={id} src={icon} />
+        <Tooltip placement="right" isOpen={this.state.tooltipOpen} target={id} toggle={this.toggle}>
+          {tipText}
+        </Tooltip>
+      </div>
+    );
+  }
+}
+
+const resistances = {
+  'phys': {
+    icon: physSvg,
+    text: "Physical",
+  },
+  'gun': {
+    icon: gunSvg,
+    text: "Gun",
+  },
+  'fire': {
+    icon: fireSvg,
+    text: "Fire",
+  },
+  'ice': {
+    icon: iceSvg,
+    text: "Ice",
+  },
+  'electric': {
+    icon: elecSvg,
+    text: "Electric",
+  },
+  'wind': {
+    icon: windSvg,
+    text: "Wind",
+  },
+  'psy': {
+    icon: psySvg,
+    text: "Psychic",
+  },
+  'nuclear': {
+    icon: nukeSvg,
+    text: 'Nuclear',
+  },
+  'bless': {
+    icon: blessSvg,
+    text: "Bless",
+  },
+  'curse': {
+    icon: curseSvg,
+    text: "Curse",
+  },
+  'wk': {
+    icon: wkSvg,
+    text: "Weak",
+  },
+  "rp": {
+    icon: rplSvg,
+    text: "Repel",
+  },
+  "rs": {
+    icon: strSvg,
+    text: "Strong",
+  },
+  "ab": {
+    icon: drSvg,
+    text: "Drain",
+  },
+  "nu": {
+    icon: nullSvg,
+    text: "Null",
+  }
+}
+
+const TypeIcon = ({ type }) => {
+  const resist = resistances[type];
+  if (resist) {
+    return <TooltipIcon icon={resist.icon} tipText={resist.text} />;
+  } else {
+    return <div>{type}</div>;
   }
 }
 
